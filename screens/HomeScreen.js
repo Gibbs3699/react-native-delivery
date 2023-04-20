0a06aee11a24a54c275b0083c805187af9772d02
Kamonpan Kaewheang <kamonpan.k@muze.co.th>
2023-04-20 14:24:45 +0700

    add image and texts to header

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,11 +1,37 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, SafeAreaView, Image} from 'react-native'
+import React, { useLayoutEffect } from 'react'
+import { useNavigation } from '@react-navigation/native';
 
 const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    // when screen mounted then do something
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerShown: false,
+        });
+    }, []);
+
   return (
-    <View>
-      <Text>HomeScreen</Text>
-    </View>
+    <SafeAreaView>
+        <View className="flex-row pb-3 items-center mx-4 space-x-2">
+            <Image 
+                source={{
+                    uri: "https://links.papareact.com/wru"
+            }}
+            className="h-7 w-7 bg-gray-300 p-4 rounded-full"
+            />
+            
+            <View>
+            <Text className="font-bold text-gray-400 text-xs">
+                Deliver now!
+            </Text>
+            <Text className="font-bold text-xl">
+                Current Location
+            </Text>
+        </View>
+        </View>
+    </SafeAreaView>
   )
 }
 
